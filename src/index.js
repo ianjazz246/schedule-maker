f56497254c1879058b489470f24358c561b81968
ianjazz246 <ianjazz246@gmail.com>
2019-05-12 11:41:01 -0700

    Schedule Output

--- src/index.js
+++ src/index.js
@@ -1,12 +1,206 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import Table from "react-bootstrap/Table";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Import react-table
+import ReactTable from "react-table";
+import "react-table/react-table.css"
+
+//react-fixed-hoc-tables
+import withFixedColumns from "react-table-hoc-fixed-columns";
+import "react-table-hoc-fixed-columns/lib/styles.css"
+
+
+import "./index.css";
+import "./schedule.js";
+
+const ReactTableFixedColumns = withFixedColumns(ReactTable);
+
+
+
+class ScheduleApp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        //to change
+        this.state = { schedules: global.schedules };
+    }
+
+    render() {
+        return (
+            <ScheduleDisplay schedules={this.state.schedules} />
+        )
+    }
+}
+
+
+
+function ScheduleRow(props) {
+    return (
+        <>
+            {console.log(props)}
+            {global.schedules.map(element => (
+                <td>
+                    {element[props.block].name}
+                </td>
+            ))}
+        </>
+    )
+}
+
+class ScheduleDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        //let headerArray = [];
+
+        //create table headers
+        /* for (let i = 0; i < global.numOfBlocks; ++i) {
+            headerArray.push(
+                <div>{i}</div>
+            );
+        } */
+
+        let tableHeaderArray = new Array(global.numOfBlocks).fill(null);
+        //create table headers
+        for (let i = 0; i < global.numOfBlocks; ++i) {
+            tableHeaderArray.push(
+                <tr>
+                    <th class="header">{i}</th>
+                    <ScheduleRow block={i} />
+                </tr>
+            );
+        }
+
+
+        /* let tempColumns = [];
+        for (let i = 1; i < global.numOfBlocks; ++i) {
+            //add column header for each block
+            tempColumns.push({
+                id: "block" + i,
+                Header: "",
+                accessor: d => d[i]
+            });
+        } */
+
+        function processData(data) {
+            let tempData = [];
+
+            for (let i = 0; i < global.numOfBlocks; ++i) {
+                //add block number beginning to each row
+                tempData.push([i + 1]);
+            }
+            
+
+            for (let schedule of data) {
+                for (let [index, course] of schedule.entries()) {
+                    tempData[index].push(course.name);
+                }
+            }
+
+            return tempData;
+        }
+
+
+
+        const data = this.props.schedules;
+
+        let columns = [];
+        for (let i = 0; i < global.numOfBlocks; ++i) {
+            columns.push(
+                {
+                Header: "Block " + i,
+                id: "block" + i,
+                accessor: d => d[i].name
+                }
+            )
+        }
+
+
+        /* const columns = [{
+            Header: "",
+            fixed: "left",
+            columns: [
+                {
+                    id: "header",
+                    Header: "Block",
+                    width: 80,
+                    accessor: d => d[0]
+                }
+            ]
+        }].concat({Header:"Classes",
+            headerClassName: "classes-header-group",
+            columns: tempColumns}); */
+
+
+        return (
+            /* <ReactTableFixedColumns
+                data={data} 
+                columns={columns} 
+                resolveData={processData} 
+                className="-striped -highlight"
+                defaultSorted={[
+                    {
+                        id: "header",
+                        desc: false
+                    }
+                ]}
+            /> */
+
+            <ReactTable
+                data={data} 
+                columns={columns} 
+                className="-striped -highlight"
+                defaultSorted={[
+                    {
+                        id: "header",
+                        desc: false
+                    }
+                ]}  
+            />
+        )
+
+
+        /* return (
+            <>
+            <div class="header">
+                {headerArray}
+            </div>
+            {global.schedules.map(element => (
+            <div class="schedule">
+                <Schedule classes={element}></Schedule>
+            </div>))}
+            </>
+        ) */
+
+
+        /* return (
+            <div class="tableContainer">
+                <Table striped hover>
+                    <tbody>
+                        <tr>
+                            <th class="header">Block</th>
+                            <td class="invisibleHeader">B</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        {tableHeaderArray}
+                    </tbody>
+                </Table>
+            </div>
+        ) */
+
+
+    }
+}
+
+class CourseInput extends React.Component {
+    
+}
+
+ReactDOM.render(
+    <ScheduleApp />,
+    document.getElementById("root")
+);
---
+++ src/schedule.js
@@ -0,0 +1,173 @@
+// class Class {
+//   //availableBlocks
+//   //array of the block numbers the class is available.
+//   constructor(name, teacher, offeredBlocks) {
+//     this.name = name;
+//     this.teacher = teacher;
+//     this.offeredBlocks = offeredBlocks;
+
+//     //add self to class schedules
+//     for (const block of offeredBlocks) {
+//       classTimes[block - 1].push(this);
+//     }
+//   }
+// }
+
+let Class = function (name, teacher, offeredBlocks) {
+    this.name = name;
+    this.teacher = teacher;
+    this.offeredBlocks = offeredBlocks;
+
+    //add self to class schedules
+    for (const block of offeredBlocks) {
+        classTimes[block - 1].push(this);
+    }
+}
+
+let classes = [
+    {
+        name: "A",
+        offeredBlocks: [1, 3, 4],
+        possibleBlocks: []
+    },
+    {
+        name: "B",
+        offeredBlocks: [2, 3],
+        possibleBlocks: []
+    },
+    {
+        name: "C",
+        offeredBlocks: [1, 4],
+        possibleBlocks: []
+    },
+    {
+        name: "D",
+        offeredBlocks: [3],
+        possibleBlocks: []
+    }
+];
+
+//number of blocks in your schedule
+let numOfBlocks = 4;
+
+//array holding makeSchedule each block and the classes in each block
+let classTimes = [];
+
+//blocks that a class must go in.
+//the class has only one available block
+let reservedBlocks = [];
+
+
+
+function setup() {
+    classTimes.length = numOfBlocks;
+    //doesn't work. Fills classTime with a reference to the same array
+    //classTimes.fill(new Array());
+
+    //need to manually fill it
+    for (let i = 0; i < numOfBlocks; ++i) {
+        classTimes[i] = [];
+    }
+
+    for (let c of classes) {
+        if (c.offeredBlocks.length < 2) {
+            //Two classes only available at same time
+            if (reservedBlocks.includes(c.offeredBlocks[0])) {
+                console.log("No possible schedule");
+                break;
+            }
+
+            let block = c.offeredBlocks[0];
+            reservedBlocks.push(block);
+            c.possibleBlocks.push(block);
+        }
+
+        for (let block of c.offeredBlocks) {
+            if (!reservedBlocks.includes(block)) {
+                c.possibleBlocks.push(block);
+            }
+        }
+    }
+}
+
+function makeSchedules(blocksPossibleClasses, currSchedule = []) {
+
+    let possibleSchedules = [];
+
+
+
+    //if only one block in blocksArray, then have reached end of recursion. Return values.
+    if (blocksPossibleClasses.length < 2) {
+        return blocksPossibleClasses;
+    }
+    else {
+        for (let aClass of blocksPossibleClasses[0]) {
+
+            //nested array of removals from each block in blocksPossibleClasses
+            let removals = [];
+            removals.length = blocksPossibleClasses.length;
+            for (let i = 0, n = blocksPossibleClasses.length; i < n; ++i) {
+                removals[i] = [];
+            }
+
+            let classPossibleSchedules = [];
+
+            //make deep copy of blocksPossibleClasses
+            //let tempPossibleSchedules = JSON.parse(JSON.stringify(blocksPossibleClasses));
+
+            //for each block, remove all occurances of the current class in the current block
+            //skipping current block.
+            for (let i = 1, n = blocksPossibleClasses.length; i < n; ++i) {
+                let m;
+                while ((m = blocksPossibleClasses[i].indexOf(aClass)) !== -1) {
+                    removals[i].push(aClass);
+                    blocksPossibleClasses[i].splice(m, 1);
+                }
+            }
+            let partialPossibleSchedules = makeSchedules(blocksPossibleClasses.slice(1));
+
+            //iterate through the returned possible schedules
+            for (let partialSchedule of partialPossibleSchedules) {
+                possibleSchedules.push([aClass].concat(partialSchedule));
+            }
+
+            for (const [i, el] of removals.entries()) {
+
+                //need to modify original blocksPossibleClasses
+
+                //blocksPossibleClasses[i] = blocksPossibleClasses[i].concat(el);
+                blocksPossibleClasses[i].push(...el);
+            }
+
+            removals = [];
+
+            //possibleSchedules.push(makeSchedules(blocksPossibleClasses.slice(1), currSchedule.push(block)));
+        }
+        return possibleSchedules;
+    }
+
+}
+
+
+//remove later
+
+setup();
+
+let classA = new Class("A", "Me", [1, 4]);
+let classB = new Class("B", "Me", [1, 3, 4]);
+let classC = new Class("C", "Me", [2]);
+let classD = new Class("D", "Me", [3, 4]);
+
+let testSchedule = [
+    [classA, classB],       //block 1
+    [classC],            //block 2
+    [classB, classD],       //block 3
+    [classA, classB, classD]   //block 4
+];
+
+
+
+var schedules = makeSchedules(testSchedule);
+//console.log(schedules);
+global.numOfBlocks = numOfBlocks;
+global.schedules = schedules;
